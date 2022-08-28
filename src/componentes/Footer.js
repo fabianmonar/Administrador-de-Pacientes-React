eb7e325872f9676d49a3772368159091fc49986e
fabi <fabian.monar.u@gmail.com>
2022-08-28 17:48:32 -0300

    first commit

---
+++ src/componentes/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.css';
+
+
+const Footer = () =>{
+
+    return(
+<div className="footer">
+    <small>Made with ♥ by fabian monar</small>
+</div>
+
+    )
+
+}
+
+export default Footer;
+Footer
